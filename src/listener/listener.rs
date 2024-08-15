a9ba48488e44a217e008136c33d685fe6a53cf79
jakkie <jakubkielbasiewicz@gmail.com>
2024-08-15 08:10:31 +0200

    listener init

--- src/listener/listener.rs
+++ src/listener/listener.rs
@@ -1,1 +1,4 @@
-use alloy::prov
+use alloy::providers::{Provider, ProviderBuilder};
+use crate::config::Config;
+use crate::db::connection::DbConn;
+pub async fn start_listener(config, db_conn)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod config;
+mod listener;
+mod db;
+mod trade;
+
+use listener::watcher::start_listener;
+
 fn main() {
-    println!("Hello, world!");
+    // Load configurations
+    let config = config::load_config();
+
+    // Initialize the database connection
+    let db_conn = db::connection::establish_connection(&config.db_url);
+
+    // Start the listener
+    start_listener(config, db_conn);
 }
